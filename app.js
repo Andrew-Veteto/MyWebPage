ddfe22f5b0ddaa6fd332d1b036f4337ba8b931c4
Andrew-Veteto <vetetoandrew@gmail>
2023-03-08 12:54:18 -0500

    added contact page

---
+++ app.js
@@ -0,0 +1,13 @@
+function showPersonalEmail() {
+    document.getElementById("personalMail").style.display = "none";
+    document.getElementById("isShown1").style.display = "block";
+    document.getElementById("EMAIL").style.marginTop = "40px";
+    document.getElementById("SocialInvites").style.marginTop = "70px";
+}
+
+function showSchoolEmail() {
+    document.getElementById("schoolMail").style.display = "none";
+    document.getElementById("isShown2").style.display = "block";
+    document.getElementById("EMAIL").style.marginTop = "40px";
+    document.getElementById("SocialInvites").style.marginTop = "70px";
+}
