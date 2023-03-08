fef93518dab7ebe7b51490a62955305c809964e2
Andrew-Veteto <vetetoandrew@gmail>
2023-03-08 16:51:10 -0500

    added about page

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 function showPersonalEmail() {
     document.getElementById("personalMail").style.display = "none";
     document.getElementById("isShown1").style.display = "block";
-    document.getElementById("EMAIL").style.marginTop = "40px";
-    document.getElementById("SocialInvites").style.marginTop = "70px";
+    document.getElementById("EMAIL").style.marginTop = "30px";
+    document.getElementById("SocialInvites").style.marginTop = "50px";
 }
 
 function showSchoolEmail() {
     document.getElementById("schoolMail").style.display = "none";
     document.getElementById("isShown2").style.display = "block";
-    document.getElementById("EMAIL").style.marginTop = "40px";
-    document.getElementById("SocialInvites").style.marginTop = "70px";
+    document.getElementById("EMAIL").style.marginTop = "30px";
+    document.getElementById("SocialInvites").style.marginTop = "50px";
 }
